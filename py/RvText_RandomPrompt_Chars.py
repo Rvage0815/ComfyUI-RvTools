ba39c70ec7951bd15cbcae71c52bec567ed9821d
rvage0815 <skentler@protonmail.com>
2024-09-27 22:18:21 +0200

    updated save image, random prompt

---
+++ py/RvText_RandomPrompt_Chars.py
@@ -0,0 +1,92 @@
+﻿import random
+import os
+
+from ..core import CATEGORY
+
+script_directory = os.path.dirname(__file__)
+folder_path = os.path.join(script_directory, "..\csv1" )
+
+def getfilename(folder):
+    name = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".csv"):
+           name.append(filename[3:-4])
+    return name
+    
+def select_random_line_from_csv_file(file, folder):
+    chosen_lines = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".csv") and filename[3:-4] == file:
+            file_path = os.path.join(folder, filename)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                if lines:
+                    chosen_lines.append(random.choice(lines).strip())
+    lines_chosed = "".join(chosen_lines)
+    return lines_chosed
+
+class RvText_RandomPrompt:
+
+    def __init__(self, seed=None):
+        if seed is not None:
+            random.seed(seed)
+
+    CATEGORY = CATEGORY.MAIN.value + CATEGORY.TEXT.value
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("prompt",)
+    FUNCTION = "create_prompt"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        required = {}
+        for filename in os.listdir(folder_path):
+              if filename.endswith(".csv"):
+                 file_path = os.path.join(folder_path, filename)
+                 lines = []
+                 with open(file_path, 'r', encoding='utf-8') as file:
+                    lines = file.readlines()
+                    required[filename [3:-4]] = (["disabled"] + ["🎲random"] + lines, {"default": "disabled"})
+        return {
+            "required": required,
+
+            "optional": {
+                "seed": ("INT", {"forceInput": True, "default": 0, "min": 0, "max": 1125899906842624}),
+            }
+        }    
+    def create_prompt(self, **kwargs):
+        name_of_files = getfilename(folder_path)
+        prompts_count = 1 #kwargs.get("Prompt_count", 0)
+        concatenated_values = ""
+        prompt_value = ""
+        final_values = ""
+        values = []
+        values = [""] * len(name_of_files)
+
+        for c in range(prompts_count):
+           for i, filename in enumerate(name_of_files):
+              if kwargs.get(filename, 0) == "🎲random":
+                     values[i] = select_random_line_from_csv_file(filename, folder_path)
+              else:      
+                     values[i] = kwargs.get(filename, 0)
+                     values[i] = values[i].strip()
+           for value in values:
+              if value != "disabled":
+                     concatenated_values += value + ","
+           print(f"➡️Prompt: {concatenated_values [:-1]}")
+           final_values += concatenated_values [:-1] + "\n" 
+           concatenated_values = ""
+
+        final_values = final_values.strip()  
+         
+        return (final_values,)
+
+NODE_NAME = 'Random Prompt: Characters // RvTools'
+NODE_DESC = 'Random Prompt: Characters'
+
+NODE_CLASS_MAPPINGS = {
+   NODE_NAME: RvText_RandomPrompt
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    NODE_NAME: NODE_DESC
+}
