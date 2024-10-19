806da7a8a594764eddb51ede5e27fec499ec26d0
Jon Carlos <webmonger@gmail.com>
2024-10-19 21:34:30 +0100

    update os.path.join to work cross platform

--- py/RvText_RandomPrompt.py
+++ py/RvText_RandomPrompt.py
@@ -4,7 +4,7 @@ import os
 from ..core import CATEGORY
 
 script_directory = os.path.dirname(__file__)
-folder_path = os.path.join(script_directory, "..\csv" )
+folder_path = os.path.join(script_directory, "..", "csv" )
 
 def getfilename(folder):
     name = []
